fb2afc0d6544a7c27165d09d9d1866d78921ed3c
AISHWARYA A NAIR <aishwaryanair440@gmail.com>
2026-01-07 18:48:35 +0530

    Add greeting and age calculation programs

--- week0/practice.py
+++ week0/practice.py
@@ -1,1 +1,10 @@
+# Program 1: Greeting the user
+name = input("What is your name? ")
+print("Hello,", name)
+
+print()  # blank line
+
+# Program 2: Age calculation
+age = int(input("Enter your age: "))
+print("Next year, you will be", age + 1)
 
